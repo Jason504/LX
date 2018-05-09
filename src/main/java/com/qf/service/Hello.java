10698c69e0eb55c4c6e23ff026785429a6bbcf92
Jason504 <ai_153687@163.com>
2018-05-09 21:12:51 +0800

    com

---
+++ src/main/java/com/qf/service/Hello.java
@@ -0,0 +1,14 @@
+package com.qf.service;
+
+/***
+ * @laifuyuan
+ * 2018/5/9  21:11 
+ *
+ **/
+
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("我是二号程序员！");
+    }
+}
